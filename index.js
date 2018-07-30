2496179347d4cf45f55b6e37ad7d91759ddc2b28
Ruchira Wageesha <ruchira.wageesha@gmail.com>
2018-07-30 08:27:29 +0530

    updating

--- index.js
+++ index.js
@@ -3,15 +3,15 @@ var serand = require('serand');
 
 dust.loadSource(dust.compile(require('./template'), 'accounts-authorized'));
 
-module.exports = function (sandbox, fn, options) {
+module.exports = function (sandbox, options, done) {
     var user = options.user;
     user.expires = user.expires - new Date().getTime();
     dust.render('accounts-authorized', options, function (err, out) {
         if (err) {
-            return;
+            return done(err);
         }
         sandbox.append(out);
-        fn(false, function () {
+        done(null, function () {
             $('.accounts-authorized', sandbox).remove();
         });
     });
